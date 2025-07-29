70926c73cb718a3b0bc39fbaf42b43a6c274a55f
anarhehest <drevv5@icloud.com>
2025-07-30 00:11:25 +0300

    upd

--- static/js/rcg.js
+++ static/js/rcg.js
@@ -1,31 +1,30 @@
-random_color = () => '#' + (Math.floor(Math.random() * 0xFFFFFF).toString(16)).padStart(6, '0');
-
-$(document).ready(function () {
-  var colorLoop = function () {
-    var frame = $(".rcg"); // for Random Color Generator
-
-    function shiftColor() {
-      const color = random_color();
-      const seconds = 2 + Math.pow(Math.PI, Math.random());
-
-      frame.css({
-        "background-color": color,
-        "box-shadow": `0 0 256px 100px ${color}`,
-        transition: `background-color ${seconds}s ease-in-out, box-shadow ${seconds}s ease-in-out`,
-      });
-
-      setTimeout(shiftColor, seconds * 10 ** 3);
+l = {
+    "random": {
+        "color": () => '#' + (Math.floor(Math.random() * 0xFFFFFF).toString(0x10)).padStart(6, '0'),
+        "seconds": () => Math.PI + Math.pow(Math.PI, Math.random()),
     }
-    shiftColor();
-  };
-  colorLoop();
-});
-
-
-function marquee() {
-  const fill = document.getElementById("marquee").textContent
-  fill.textContent = fill.repeat(Math.ceil((window.innerWidth / (5)) * 2))
 }
 
-window.addEventListener('load', marquee);
-window.addEventListener('resize', marquee);
+$(document).ready(function (frame) {
+    var colorLoop = function (frame) {
+        function shiftColor() {
+            const color = l.random.color();
+            const shadow = window.innerHeight / 10;
+            const time = l.random.seconds();
+
+            frame.css({
+                "background-color": color,
+                "box-shadow": `0px 0px ${shadow}px 0px ${color}`,
+                "transition": `background-color ${time}s ease-in-out, box-shadow ${time}s ease-in-out`,
+            });
+
+            setTimeout(shiftColor, time * 10 ** 3);
+        };
+
+        shiftColor();
+    };
+
+    var frame = $(".rcg");
+
+    colorLoop(frame);
+});
